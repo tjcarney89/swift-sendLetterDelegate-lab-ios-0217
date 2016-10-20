06cb47af65d2f0787161c7d2e3b4087bbf720299
joelconnects <joelconnects@gmail.com>
2016-10-20 16:11:12 -0400

    first commit

---
+++ SendLetterDelegate/LondonViewController.swift
@@ -0,0 +1,151 @@
+//
+//  ViewController.swift
+//  SendPackageDelegate
+//
+//  Created by Joel Bell on 10/18/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+import UIKit
+
+protocol LondonViewControllerDelegate: class {
+    func letterSent(from sender: LondonViewController, message: String)
+}
+
+class LondonViewController: UIViewController, UITextViewDelegate {
+    
+    // Protocol
+    weak var delegate: LondonViewControllerDelegate?
+    
+    // View elements
+    @IBOutlet weak var letterTextView: UITextView!
+    @IBOutlet weak var letterHeaderLabel: UILabel!
+    @IBOutlet weak var containerViewCenterYConstraint: NSLayoutConstraint!
+    @IBOutlet weak var sendButton: UIButton!
+    
+    // Constraints for animation
+    @IBOutlet weak var letterTextViewCenterXConstraint: NSLayoutConstraint!
+    @IBOutlet weak var letterImageViewCenterXConstraint: NSLayoutConstraint!
+    
+    // Keyboard height for adjusting view elements
+    var keyboardHeight: CGFloat = 0
+    
+    
+    // MARK: View Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        sendButton.isHidden = true
+        addKeyboardNotificationObservers()
+        letterTextView.layer.cornerRadius = 8
+    }
+    
+    // MARK: Actions
+    
+    @IBAction func sendButtonTapped(_ sender: UIButton) {
+        self.delegate?.letterSent(from: self, message: letterTextView.text)
+        
+        animateLetter {
+            self.dismiss(animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func tapGestureTapped(_ sender: UITapGestureRecognizer) {
+        validateTextView()
+        letterTextView.resignFirstResponder()
+    }
+    
+    // MARK: Animation
+    
+    func animateLetter(completion: @escaping ()->()) {
+        
+        UIView.animateKeyframes(withDuration: 0.5, delay: 0.0, options: [], animations: {
+            
+            UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 0.1, animations: {
+                
+                self.letterImageViewCenterXConstraint.constant = 30
+                self.letterTextViewCenterXConstraint.constant = 15
+                self.view.layoutIfNeeded()
+                
+            })
+            UIView.addKeyframe(withRelativeStartTime: 0.1, relativeDuration: 0.9, animations: {
+                
+                self.letterTextViewCenterXConstraint.constant = -self.view.frame.width
+                self.view.layoutIfNeeded()
+                
+            })
+            
+        }) { _ in
+            
+            completion()
+            
+        }
+    }
+    
+    // MARK: Notifications
+    
+    func addKeyboardNotificationObservers() {
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard(notification:)), name: Notification.Name.UIKeyboardWillShow, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard(notification:)), name: Notification.Name.UIKeyboardWillHide, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard(notification:)), name: Notification.Name.UIKeyboardWillChangeFrame, object: nil)
+    }
+    
+    func adjustForKeyboard(notification: Notification) {
+        
+        switch notification.name.rawValue {
+        case "UIKeyboardWillChangeFrameNotification":
+            let userInfo = notification.userInfo! as NSDictionary
+            let keyboardFrame = userInfo.value(forKey: UIKeyboardFrameEndUserInfoKey) as! NSValue
+            let keyboardRectangle = keyboardFrame.cgRectValue
+            keyboardHeight = keyboardRectangle.height
+        case "UIKeyboardWillShowNotification":
+            self.containerViewCenterYConstraint.constant = -self.keyboardHeight/2
+            self.view.layoutIfNeeded()
+        case "UIKeyboardWillHideNotification":
+            letterHeaderLabel.text = ""
+            self.containerViewCenterYConstraint.constant = 0
+            self.view.layoutIfNeeded()
+        default:
+            break
+        }
+        
+    }
+    
+    // MARK: Validation
+    
+    func validateTextView() {
+        
+        if letterTextView.text.isEmpty {
+            
+            let alertController = UIAlertController(title: "How Rude!", message: "Be a decent human being and send a thank you letter", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alertController.addAction(okAction)
+            present(alertController, animated: true, completion: nil)
+            
+        } else {
+            sendButton.isHidden = false
+            letterTextView.resignFirstResponder()
+        }
+    }
+    
+    // MARK: TextView Delegate
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+    
+        if text == "\n" {
+            validateTextView()
+        }
+        return true
+    }
+    
+    func textViewShouldBeginEditing(_ textView: UITextView) -> Bool {
+        if textView.text == "Write a thank you letter..." {
+            textView.text = ""
+        }
+        return true
+    }
+
+}
+
---
+++ SendLetterDelegate/NewYorkViewController.swift
@@ -0,0 +1,102 @@
+//
+//  ViewController.swift
+//  SendPackageDelegate
+//
+//  Created by Joel Bell on 10/18/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+import UIKit
+
+class NewYorkViewController: UIViewController, LondonViewControllerDelegate {
+    
+    // View elements
+    @IBOutlet weak var receivedHeaderLabel: UILabel!
+    @IBOutlet weak var packageImageView: UIImageView!
+    @IBOutlet weak var letterTextView: UITextView!
+    @IBOutlet weak var sendButton: UIButton!
+    
+    // Constraints for animation
+    @IBOutlet weak var packageCenterXConstraint: NSLayoutConstraint!
+    @IBOutlet weak var packageCenterYConstraint: NSLayoutConstraint!
+    
+    // MARK: View Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        receivedHeaderLabel.isHidden = true
+        letterTextView.isHidden = true
+    }
+    
+    // MARK: Action
+    
+    @IBAction func sendButton(_ sender: UIButton) {
+        animatePackage {
+            self.performSegue(withIdentifier: "sentSegue", sender: nil)
+        }
+    }
+    
+    // MARK: Segue
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "sentSegue" {
+            let londonViewController = segue.destination as? LondonViewController
+            londonViewController?.delegate = self
+        }
+        
+    }
+    
+    // MARK: Animation
+    
+    func animatePackage(completion: @escaping ()->()) {
+        
+        UIView.animateKeyframes(withDuration: 0.7, delay: 0.0, options: [.calculationModeCubic], animations: {
+            
+            UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 0.1, animations: {
+                
+                self.packageCenterYConstraint.constant = 10
+                self.packageCenterXConstraint.constant = 10
+                self.view.layoutIfNeeded()
+                
+            })
+            UIView.addKeyframe(withRelativeStartTime: 0.1, relativeDuration: 0.4, animations: {
+                
+                self.packageCenterYConstraint.constant = -(self.view.frame.size.height/4)
+                self.packageCenterXConstraint.constant = -(self.view.frame.size.width * 0.1)
+                self.packageImageView.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
+                self.view.layoutIfNeeded()
+                
+            })
+            
+            UIView.addKeyframe(withRelativeStartTime: 0.5, relativeDuration: 0.5, animations: {
+                
+                self.packageCenterXConstraint.constant = self.view.frame.size.width/2
+                self.packageCenterYConstraint.constant = -self.view.frame.size.height/2
+                self.packageImageView.transform = CGAffineTransform(scaleX: 0.0, y: 0.0)
+                self.view.layoutIfNeeded()
+                
+            })
+            
+        }) { _ in
+            
+            completion()
+        }
+        
+    }
+    
+    // MARK: Delegate
+    
+    func letterSent(from sender: LondonViewController, message: String) {
+        
+        letterTextView.text = message
+        packageImageView.isHidden = true
+        receivedHeaderLabel.isHidden = false
+        letterTextView.isHidden = false
+        sendButton.isHidden = true
+    
+    }
+
+}
+
